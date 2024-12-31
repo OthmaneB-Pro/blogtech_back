1137b912f70c4369fc671be3cfaaa8031568dd21
OthmaneB-Pro <othmane93.baar@gmail.com>
2024-12-31 21:45:27 +0100

    first commit initialize

---
+++ src/main/java/blogtech/blogtech/BlogtechApplication.java
@@ -0,0 +1,13 @@
+package blogtech.blogtech;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlogtechApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BlogtechApplication.class, args);
+	}
+
+}
